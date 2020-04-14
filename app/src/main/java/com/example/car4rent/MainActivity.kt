90599d0ede281a0bf2454c446b9b6ab28f82a049
Acer <weiyangtan1999@gmail.com>
2020-04-15 03:01:33 +0800

    test

---
+++ app/src/main/java/com/example/car4rent/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.car4rent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //testing tan
+    }
+}
